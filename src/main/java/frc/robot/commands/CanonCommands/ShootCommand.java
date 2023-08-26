79afb20bba4e2740fa97c008953cbc8e5b200f17
Crusaderbots <crusaderbots@gmail.com>
2023-08-26 14:52:44 -0700

    checkup

--- src/main/java/frc/robot/commands/CanonCommands/ShootCommand.java
+++ src/main/java/frc/robot/commands/CanonCommands/ShootCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.CanonCommands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.CanonSubsystem;
@@ -7,9 +8,9 @@ import frc.robot.subsystems.CanonSubsystem;
 public class ShootCommand extends SequentialCommandGroup {
     public ShootCommand (CanonSubsystem canonSubsystem) {
         addCommands(
-            new FireCommand(canonSubsystem),
+            new InstantCommand(() -> canonSubsystem.Fire()),
             new WaitCommand(.5),
-            new CloseCommand(canonSubsystem)
+            new InstantCommand(() -> canonSubsystem.Close())
         );
     }
 }
